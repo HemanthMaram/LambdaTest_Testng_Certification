b2630471e49c06fa0c2b3408d00615956f0d6eae
Maram Hemanth Reddy <maram_reddy@persistent.com>
2025-02-14 10:50:03 +0530

    chanhges

--- src/test/java/com/lambdaTests/Test/BaseClass.java
+++ src/test/java/com/lambdaTests/Test/BaseClass.java
@@ -1,55 +1,106 @@
 package com.lambdaTests.Test;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
+////import org.openqa.selenium.*;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.remote.DesiredCapabilities;
+//import org.openqa.selenium.remote.RemoteWebDriver;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//import org.testng.annotations.*;
+//import java.net.MalformedURLException;
+//import java.net.URL;
 import java.time.Duration;
+//import java.util.HashMap;
+//
+//public class BaseClass {
+//    protected WebDriver driver;
+//    protected WebDriverWait wait;
+//    String UserName = "hmaram21";
+//    String AccessToken ="OMjdH4yKzUrrHksRqZiNx0crd51IXnfb2YBQSJ8b2qNbu91APw";
+//    @BeforeMethod
+//    @Parameters({"browser", "browserVersion", "platform"})
+//    public void setUp(String browser, String browserVersion, String platform) throws MalformedURLException {
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//        capabilities.setCapability("browserName", browser);
+//        capabilities.setCapability("browserVersion", browserVersion);
+//        capabilities.setCapability("platform", platform);
+//        capabilities.setCapability("LT:Options", new HashMap<String, Object>() {{
+//            put("username", UserName);
+//            put("accessKey", AccessToken);
+//            put("resolution", "1920x1080");
+//            put("build", "TestBuild");
+//            put("name", "testng");
+//            put("network", true);
+//            put("video", true);
+//            put("console", true);
+//        }});
+//
+//        driver = new RemoteWebDriver(new URL("https://"+UserName+":"+AccessToken+"@hub.lambdatest.com/wd/hub"), capabilities);
+//        driver.get("https://www.lambdatest.com/selenium-playground/");
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+//
+//
+//    }
+//
+//    @AfterMethod
+//    public void tearDown() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+//    }
+//}
 
-public class BaseClass {
-    protected WebDriver driver;
-    protected WebDriverWait wait;
+import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    @BeforeMethod
-    @Parameters({"browser", "url"})
-    public void setUp(String browser, String url) {
-        switch (browser.toLowerCase()) {
-            case "chrome":
-                WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
-                break;
-            case "edge":
-                WebDriverManager.edgedriver().setup();
-                driver = new EdgeDriver();
-                break;
-            case "firefox":
-                WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
-                break;
-            case "internet explorer":
-                WebDriverManager.iedriver().setup();
-                driver = new InternetExplorerDriver();
-                break;
-            default:
-                throw new IllegalArgumentException("Browser " + browser + " not supported.");
+
+public class BaseClass{
+    WebDriver driver = null;
+
+    public static String status = "passed";
+    String username = "hmaram21";
+    String access_key = "OMjdH4yKzUrrHksRqZiNx0crd51IXnfb2YBQSJ8b2qNbu91APw";
+
+
+
+    @BeforeTest
+    @Parameters(value={"browser","version","platform"})
+    public void testSetUp(String browser, String version, String platform) throws Exception
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("build", "[LambdaTest] Demonstration of Selenium Automation Testing");
+        capabilities.setCapability("name", "[LambdaTest] Demonstration of Selenium Automation Testing");
+        capabilities.setCapability("platform", platform);
+        capabilities.setCapability("browserName", browser);
+        capabilities.setCapability("version",version);
+        capabilities.setCapability("tunnel",false);
+        capabilities.setCapability("network",true);
+        capabilities.setCapability("console",true);
+        capabilities.setCapability("visual",true);
+
+        try
+        {
+            driver = new RemoteWebDriver(new URL("https://" + username + ":" + access_key + "@hub.lambdatest.com/wd/hub"), capabilities);
+                }
+        catch (MalformedURLException e)
+        {
+            System.out.println("Invalid grid URL");
         }
-
-        driver.get(url);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-//        new WebDriverWait(driver, Duration.ofSeconds(10))
+        System.out.println("Started session");
     }
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+
+@AfterTest
+public void tearDown()
+{
+    if (driver != null)
+    {
+        ((JavascriptExecutor) driver).executeScript("lambda-status=" + status);
+        driver.quit();
     }
+}
 }
--- src/test/java/com/lambdaTests/Test/TestScenario1.java
+++ src/test/java/com/lambdaTests/Test/TestScenario1.java
@@ -1,11 +1,12 @@
 package com.lambdaTests.Test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class TestScenario1 extends BaseClass {
-
+    WebDriverWait wait;
     @Test
     public void validatePageTitle() {
         wait.until(driver -> driver.findElement(By.tagName("body")));
